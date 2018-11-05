4d037e70536d4a3d03c13a09bbb41f6d8c73ba77
ponpppher <genmaier@gmail.com>
2018-11-05 16:34:21 +0900

    asset_clean and compile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
