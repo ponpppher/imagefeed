85dfbb37f05fb8d1610547b56821b2e7ba4d12d9
ponpppher <genmaier@gmail.com>
2018-12-13 20:00:57 +0900

    del assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,13 @@ CarrierWave.configure do |config|
 
   config.storage :fog
   config.fog_provider = 'fog/aws'
+
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    provider:              'AWS',
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1',
-    path_style: true,
+    region:                'ap-northeast-1',
+    path_style:            true,
   }
 
   config.fog_public = true
